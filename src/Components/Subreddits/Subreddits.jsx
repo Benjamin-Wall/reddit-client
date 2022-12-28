445b8d49ad20d139b991c1c04867492200ae24af
Benjamin Wall <benwallmain@outlook.com>
2022-12-28 00:25:37 +0000

    added logic to give current subreddit the active class

--- src/Components/Subreddits/Subreddits.jsx
+++ src/Components/Subreddits/Subreddits.jsx
@@ -1,13 +1,18 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
 import { Link } from 'react-router-dom';
 
 import '../../assets/css/card.css';
 import '../../assets/css/subreddits.css';
 
+import { selectSelectedSubreddit } from '../../Features/reddit/redditSlice';
+
 export const Subreddits = ({ name }) => {
+  const selectedSubreddit = useSelector(selectSelectedSubreddit);
+
   return (
     <>
-      <li className={name === 'r/Home' ? 'selected-subreddit' : 'false'}>
+      <li className={selectedSubreddit === name ? 'selected-subreddit' : 'false'}>
         <button type="button">
           <Link to={`/${name}`}>
             <img
