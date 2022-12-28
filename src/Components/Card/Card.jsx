8e6492cf42545a5c04c53088adab3bb2b29c8e9a
Benjamin Wall <benwallmain@outlook.com>
2022-12-28 01:38:15 +0000

    added placeholder values

--- src/Components/Card/Card.jsx
+++ src/Components/Card/Card.jsx
@@ -30,7 +30,7 @@ export const Card = ({ title, author, num_comments, url, upvotes }) => {
               <path d="M12 21c-1.654 0-3-1.346-3-3v-4.764c-1.143 1.024-3.025.979-4.121-.115-1.17-1.169-1.17-3.073 0-4.242l7.121-7.121 7.121 7.121c1.17 1.169 1.17 3.073 0 4.242-1.094 1.095-2.979 1.14-4.121.115v4.764c0 1.654-1.346 3-3 3zm-1-12.586v9.586c0 .551.448 1 1 1s1-.449 1-1v-9.586l3.293 3.293c.379.378 1.035.378 1.414 0 .391-.391.391-1.023 0-1.414l-5.707-5.707-5.707 5.707c-.391.391-.391 1.023 0 1.414.379.378 1.035.378 1.414 0l3.293-3.293z"></path>
             </svg>
           </button>
-          <p className="post-votes-value ">{upvotes.toLocaleString()}</p>
+          <p className="post-votes-value ">{upvotes ? upvotes.toLocaleString() : '0'}</p>
           <button
             type="button"
             className="icon-action-button down-vote false"
@@ -53,12 +53,12 @@ export const Card = ({ title, author, num_comments, url, upvotes }) => {
           </button>
         </div>
         <div className="post-container">
-          <h3 className="post-title">{title}</h3>
+          <h3 className="post-title">{title ? title : 'Placeholder Title'}</h3>
           <div className="post-image-container">
             <img
-              src={url}
+              src={url ? url : 'https://via.placeholder.com/300'}
               className="post-image"
-              alt={title}
+              alt={title ? title : 'Placeholder Title'}
             />
           </div>
           <div className="post-details">
@@ -68,7 +68,7 @@ export const Card = ({ title, author, num_comments, url, upvotes }) => {
                 alt="Blue reddit icon"
                 className="avatar-profile-image"
               ></img>
-              <span className="author-username">{author}</span>
+              <span className="author-username">{author ? author : 'Placeholder Author'}</span>
             </span>
             <span className="post-comments-container">
               <button
@@ -91,7 +91,7 @@ export const Card = ({ title, author, num_comments, url, upvotes }) => {
                   <path d="M18 7c.542 0 1 .458 1 1v7c0 .542-.458 1-1 1h-8.829l-.171.171v-.171h-3c-.542 0-1-.458-1-1v-7c0-.542.458-1 1-1h12m0-2h-12c-1.65 0-3 1.35-3 3v7c0 1.65 1.35 3 3 3h1v3l3-3h8c1.65 0 3-1.35 3-3v-7c0-1.65-1.35-3-3-3z"></path>
                 </svg>
               </button>
-              {num_comments}
+              {num_comments ? num_comments : '0'}
             </span>
           </div>
         </div>
